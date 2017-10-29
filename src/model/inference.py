01f6082f9483241814996a3e3396b5d32608e8b1
Kakuro1 <16452006+Kakuro1@users.noreply.github.com>
2017-10-29 20:30:59 +0100

    branch creation with model draft

--- src/model/inference.py
+++ src/model/inference.py
@@ -3,3 +3,9 @@
 '''inference.py: Implement the network in tensorflow to make predictions.'''
 
 ################################################################################
+
+import tensorflow as tf
+from ..helper.tf_layers_nets import fc_network
+
+def inference(x, layer_sizes=[27, 50, 50, 1], name="FFN", act='relu'):
+    return fc_network(x, layer_sizes, name, act)
--- src/model/loss.py
+++ src/model/loss.py
@@ -3,3 +3,17 @@
 '''loss.py: Implement the loss function used for training.'''
 
 ################################################################################
+
+import tensorflow as tf
+
+def loss(logits, labels):
+    """Calculates the loss from the logits and the labels.
+
+    Args:
+        logits: Logits tensor, float - [batch_size].
+        labels: Labels tensor, int32 - [batch_size].
+
+    Returns:
+        loss: Loss tensor of type float.
+    """
+  return tf.reduce_sum(tf.square(logits - labels))
